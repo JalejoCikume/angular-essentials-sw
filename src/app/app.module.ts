3201027e4f967d06a1d2de246cda5fc622da25ae
Josue Alejo <jalejo@cikume.com>
2022-01-13 15:39:32 -0600

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
+import { StarWarsService } from './star-wars.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ListComponent } from './list/list.component';
+import { ItemComponent } from './item/item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    ListComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [StarWarsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/item/item.component.ts
@@ -0,0 +1,31 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { StarWarsService } from '../star-wars.service';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  @Input() character: any;
+  @Output() sideAssigned = new EventEmitter<{ name: string, side: string }>();
+  swService: StarWarsService;
+
+  constructor(swService: StarWarsService) {
+    this.swService = swService;
+  }
+
+  ngOnInit(): void {
+  }
+
+  onAssign(side) {
+    //To replace on next lectures
+    // this.character.side = side;
+    // this.sideAssigned.emit({name: this.character.name, side})
+
+    this.swService.onSideChosen({ name: this.character.name, side })
+  }
+
+}
